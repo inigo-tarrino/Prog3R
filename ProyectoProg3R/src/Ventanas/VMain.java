3d1286b61275aa8d7811e3197fb122557569aeac
Ander Artola <ander.artola@opendeusto.es>
2019-10-09 15:57:26 +0200

    VMain a falta de retocar

--- ProyectoProg3R/src/Ventanas/VMain.java
+++ ProyectoProg3R/src/Ventanas/VMain.java
@@ -1,23 +1,156 @@
 package Ventanas;
 
-import javax.swing.JFrame;
-import javax.swing.*;
+import java.awt.EventQueue;
 
-public class VMain extends JFrame
-{
-	protected JPanel panelprinc;
-	protected JTextField usuario;
-	protected JTextField contra;
-	
-	public VMain() 
-	{
-		
-	
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+
+public class VMain {
+
+	private JFrame ventanaMain;
+	private final Action actionHome = new SwingAction();
+	private final Action actionShop = new SwingAction_1();
+	private final Action actionAccount = new SwingAction_2();
+	private final Action actionHistory = new SwingAction_3();
+	private final Action actionPreferences = new SwingAction_4();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VMain window = new VMain();
+					window.ventanaMain.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
-	public static void main(String[] args)
-	{
+	/**
+	 * Create the application.
+	 */
+	public VMain() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		ventanaMain = new JFrame();
+		ventanaMain.setBounds(100, 100, 1280, 720);
+		ventanaMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ventanaMain.getContentPane().setLayout(null);
 		
+		JPanel pBotonera= new JPanel();
+		pBotonera.setBounds(0, 0, 350, 698);
+		ventanaMain.getContentPane().add(pBotonera);
+		pBotonera.setLayout(null);
+		
+		JButton bHome = new JButton("Home");
+		bHome.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JPanel pHome = new JPanel();
+				pHome.setBounds(0, 0, 360, 900);
+				ventanaMain.getContentPane().add(pHome);
+				pHome.setLayout(null);
+				System.out.println("pHome");
+			}
+		});
+		bHome.setAction(actionHome);
+		bHome.setBounds(10, 10, 250, 50);
+		pBotonera.add(bHome);
+		
+		JButton bShop = new JButton("Shop");
+		bShop.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		bShop.setAction(actionShop);
+		bShop.setBounds(10, 70, 250, 50);
+		pBotonera.add(bShop);
+		
+		JButton bAccount = new JButton("Account");
+		bAccount.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		bAccount.setAction(actionAccount);
+		bAccount.setBounds(10, 510, 250, 50);
+		pBotonera.add(bAccount);
+		
+		JButton bHistory = new JButton("History");
+		bHistory.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		bHistory.setAction(actionHistory);
+		bHistory.setBounds(10, 570, 250, 50);
+		pBotonera.add(bHistory);
+		
+		JButton bPreferences = new JButton("Preferences");
+		bPreferences.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		bPreferences.setAction(actionPreferences);
+		bPreferences.setBounds(10, 630, 250, 50);
+		pBotonera.add(bPreferences);
+	}
+	private class SwingAction extends AbstractAction {
+		public SwingAction() {
+			putValue(NAME, "Home");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	private class SwingAction_1 extends AbstractAction {
+		public SwingAction_1() {
+			putValue(NAME, "Shop");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	private class SwingAction_2 extends AbstractAction {
+		public SwingAction_2() {
+			putValue(NAME, "Account");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	private class SwingAction_3 extends AbstractAction {
+		public SwingAction_3() {
+			putValue(NAME, "History");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	private class SwingAction_4 extends AbstractAction {
+		public SwingAction_4() {
+			putValue(NAME, "Preferences");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
 	}
 }
-
