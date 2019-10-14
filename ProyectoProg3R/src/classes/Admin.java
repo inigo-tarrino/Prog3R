f3aba8c1aa3e78491ea33c7e384694b278e907ea
eneko98 <valeroeneko@gmail.com>
2019-10-14 12:47:54 +0200

    añadidos atributos de la clase usuario + sets y gets y contructor de la clase y de la de admin

--- ProyectoProg3R/src/classes/Admin.java
+++ ProyectoProg3R/src/classes/Admin.java
@@ -2,4 +2,9 @@ package classes;
 
 public class Admin extends User {
 
+	public Admin(String nickName, String pass, String email, String direccion) {
+		super(nickName, pass, email, direccion);
+		// TODO Auto-generated constructor stub
+	}
+
 }
--- ProyectoProg3R/src/classes/User.java
+++ ProyectoProg3R/src/classes/User.java
@@ -1,5 +1,42 @@
 package classes;
 
 public class User {
-
+	String nickName;
+	String pass;
+	String email;
+	String direccion;
+	boolean isAdmin;
+	
+	public User(String nickName, String pass, String email, String direccion) {
+		super();
+		this.nickName = nickName;
+		this.pass = pass;
+		this.email = email;
+		this.direccion = direccion;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
 }
